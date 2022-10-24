1152230f857ea6bd769f74dbe6f359017246523c
Haoqing Wang <wang.haoqi@northeastern.edu>
2022-10-23 21:16:27 -0400

    push new things

--- browser-app/src/App.js
+++ browser-app/src/App.js
@@ -1,8 +1,9 @@
 import { Breadcrumb, Layout, Card, Menu } from 'antd';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import PostList from "./Post/List";
+import UserList from "./User/List";
 
 const { Header, Content, Footer } = Layout;
 
@@ -10,22 +11,27 @@ const App = () => (
   <Router>
     <Layout className="layout">
       <Header style={{color: "#ffffff"}}>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={new Array(5).fill(null).map((_, index) => {
-            const key = index + 1;
-            if (index === 0) {
-              return { key, label: "E6156 Sprint" }
-            } else {
-              return { key, label: `nav ${key}`}
-            }
-          })}
-        />
+          E6156 Sprint1
       </Header>
+        <Content>
+            <Menu
+                style={{ marginright: 'auto' }}
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}>
+                <Menu.Item key="1">
+                    <span>Posts</span>
+                    <Link to="/" />
+                </Menu.Item>
+                <Menu.Item key="2">
+                    <span>Users</span>
+                    <Link to="/user" />
+                </Menu.Item>
+            </Menu>
+        </Content>
       <Routes>
         <Route exact path="/" element={<PostList/>}/>
+          <Route exact path="/user" element={<UserList/>}/>
       </Routes>
 
       <Footer
--- browser-app/src/Post/List.js
+++ browser-app/src/Post/List.js
@@ -1,10 +1,14 @@
 import { Layout, Card } from 'antd';
 import React from 'react';
+import request from "../utils/request";
 
 function PostList() {
-return(
-    <div>something</div>
-)
+    let rows = request("http://127.0.0.1:5000/comment/query?postId=1")
+    console.log(rows)
+    return(
+        <div>
+        </div>
+    )
 }
 
 export default PostList;
