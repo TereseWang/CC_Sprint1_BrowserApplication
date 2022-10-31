0d26825cf788c17db547d9aea87d2f3111628a02
hzzhang <monsttrod@gmail.com>
2022-10-30 22:26:54 -0400

    add detail page UI

--- browser-app/src/Detail/index.js
+++ browser-app/src/Detail/index.js
@@ -1,4 +1,5 @@
-import { Layout, Card } from 'antd';
+import { Layout, Card, Comment, Tooltip, List } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import API from '../services/apis';
@@ -9,17 +10,54 @@ function PostDetail() {
   const [comments, setComments] = useState([])
 
   useEffect(() => {
+    console.log(postId)
     const commentData = API.CommentListByPostId
-    // const commentCards = commentData.map(comment => 
-      
-    //   )
-    
-  }, [])
+    const commentCards = commentData.map(comment => ({
+      key: comment.commendId,
+      actions: [<span key="comment-list-reply-to-0">Reply to</span>],
+      author: "Han Solo",
+      avatar: "https://joeschmoe.io/api/v1/random",
+      content: (<p>{comment.content}</p>),
+      datetime: (<Tooltip title="2022-10-30 22:22:33"><span>9 hours ago</span></Tooltip>)
+    }))
+
+    setComments(commentCards)
+  }, [postId])
+
+  const post = { title: "A Sample Title", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sapien ut elit posuere vestibulum. Fusce non faucibus sem, non tristique nulla. Nulla quis posuere libero. Vestibulum dui leo, interdum vitae gravida sit amet, sagittis vel justo. Nam nec sagittis nulla, vel rutrum ante. Morbi viverra nibh a odio sodales tincidunt. Nulla sed arcu nibh. Duis volutpat mattis elit quis commodo. Fusce quis ultricies diam. Curabitur sit amet magna sagittis, varius est vel, dapibus purus. Aliquam accumsan luctus nibh, eu rhoncus libero ornare pharetra. Proin posuere metus ut velit auctor pulvinar. Sed aliquet auctor est et sollicitudin. Maecenas quis leo in eros placerat mattis at id mi. Maecenas ut iaculis mi. Maecenas finibus massa erat, id molestie ante finibus ac." }
 
   return (
-    <div>
-      {postId} test
-    </div>
+    <Layout>
+      <Content>
+        <Card
+          title={post.title}
+          bordered={true} 
+          style={{ margin: "50px" }}
+        >
+          <p>{post.content}</p>
+        </Card>
+        <List
+          className="comment-list"
+          style={{marginLeft: "80px", marginRight: "80px"}}
+          header={`${comments.length} replies`}
+          itemLayout="horizontal"
+          dataSource={comments}
+          renderItem={(item) => (
+            <li>
+              <Comment
+                style={{background: "white", padding: "10px"}}
+                actions={item.actions}
+                author={item.author}
+                avatar={item.avatar}
+                content={item.content}
+                datetime={item.datetime}
+              />
+            </li>
+          )}    
+        >
+        </List>
+      </Content>
+    </Layout>
   )
 }
 
--- browser-app/src/services/apis.js
+++ browser-app/src/services/apis.js
@@ -5,7 +5,10 @@ const API = {
     { pid: 3, uid: 6, title: "maybe later", post: "more random text messages." },
   ],
   CommentListByPostId: [
-    {commendId: 1, postId: 2, content: "some content"},
+    {commendId: 1, postId: 2, content: `We supply a series of design principles, 
+    practical patterns and high quality design
+    resources (Sketch and Axure), to help people create their product prototypes beautifully and
+    efficiently.`},
   ]
 }
 
